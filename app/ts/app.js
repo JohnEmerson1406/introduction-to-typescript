4d22ac92aebe9e7d95f5b786216543b00e197cef
john-emerson14 <john.carv.sousa@gmail.com>
2019-12-14 18:38:25 -0300

    refactor: add some codes to app.ts

--- app/ts/app.js
+++ app/ts/app.js
@@ -1,3 +1,47 @@
-var nome = "Type Script";
-console.log("Hello, " + nome);
+var ts = "Type Script";
+console.log("Hello, " + ts);
+var a = 10;
+console.log(a); //saída: undefined
+var b = 10;
+console.log(typeof (b)); //saída: number
+var c = 10;
+c = "john";
+console.log(c); //saída: john
+// boolean:
+var isDone = false;
+isDone = true;
+// number: engloba os tipos inteiros e reais
+var inteiro = 6;
+var real = 4.8;
+var hex = 0xf0a0d;
+var binario = 42;
+var octal = 228;
+// string: cadeias de caracteres delimitadas por “” e ‘’
+var color = "azul";
+color = 'vermelho' + " escuro";
+console.log(color);
+/* Podem ter múltiplas linhas sem necessidade de
+caracteres de escape;
+Também podem conter expressões;
+São delimitas por crases ``: */
+var nome = "John Emerson";
+var idade = 19;
+var frase = "Meu nome \u00E9 " + nome + ".\n\nCompletarei " + (idade + 1) + " ano que vem.";
+console.log(frase);
+/* Como Java ou C#, são maneiras de fornecer
+nomes para conjuntos de valores numéricos: */
+var Cores;
+(function (Cores) {
+    Cores[Cores["Vermelho"] = 0] = "Vermelho";
+    Cores[Cores["Verde"] = 1] = "Verde";
+    Cores[Cores["Azul"] = 2] = "Azul";
+})(Cores || (Cores = {}));
+var d = Cores.Verde;
+console.log(d); // 1
+console.log(Cores[2]); //azul
+var numeros1 = [1, 2, 3];
+var numeros2 = [1, 2, 3];
+var numeros3 = [1, 2, 3];
+numeros3.push(4);
+console.log(numeros3.reverse()); // [4,3,2,1]
 //# sourceMappingURL=app.js.map
--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,2 +1,53 @@
-let nome : string = "Type Script"
-console.log("Hello, " + nome);
+let ts : string = "Type Script"
+console.log("Hello, " + ts);
+
+let a : number = 10;
+console.log(a); //saída: undefined
+
+let b = 10;
+console.log(typeof(b)); //saída: number
+
+let c : any = 10;
+c = "john";
+console.log(c); //saída: john
+
+// boolean:
+let isDone: boolean = false;
+isDone = true;
+
+// number: engloba os tipos inteiros e reais
+let inteiro: number = 6;
+let real: number = 4.8;
+let hex: number = 0xf0a0d;
+let binario: number = 0b101010;
+let octal: number = 0o344;
+
+// string: cadeias de caracteres delimitadas por “” e ‘’
+let color: string = "azul";
+color = 'vermelho' + " escuro";
+console.log(color);
+
+/* Podem ter múltiplas linhas sem necessidade de
+caracteres de escape;
+Também podem conter expressões;
+São delimitas por crases ``: */
+let nome : string = "John Emerson";
+let idade: number = 19;
+let frase: string = `Meu nome é ${nome}.
+
+Completarei ${ idade + 1 } ano que vem.`;
+console.log(frase);
+
+/* Como Java ou C#, são maneiras de fornecer
+nomes para conjuntos de valores numéricos: */
+enum Cores {Vermelho, Verde, Azul}
+let d: Cores = Cores.Verde;
+
+console.log(d); // 1
+console.log(Cores[2]) //azul
+
+let numeros1: number[] = [1, 2, 3];
+let numeros2: Array<number> = [1, 2, 3];
+let numeros3: number[] = [1, 2, 3];
+numeros3.push(4);
+console.log(numeros3.reverse()); // [4,3,2,1]
